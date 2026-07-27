cac02e7cbec39cbcfd4721811da0bb1a7b220000
Yaasirakin <yasirakinfenwa@gmail.com>
2026-07-27 07:24:18 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Signup from "./pages/Signup";
+const App = () => {
+  return (
+    <>
+      <Hero/>
+      <Signup/>
+    </>
+  );
+}
+export default App;
